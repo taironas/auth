44f587e500232d1400772aa584158ef0f67ccdb2
santiaago <santiago.ariassar@gmail.com>
2015-06-15 21:21:00 +0100

    code review tests

--- auth_test.go
+++ auth_test.go
@@ -1,24 +1,25 @@
 package auth
 
 import "testing"
-import "fmt"
 
-func TestGoogle(t *testing.T) {
-	// test with an empty ID token
-	fmt.Println("Test with an empty ID token")
-	user, err := Google("")
-	if err == nil {
+func TestGoogleWithEmptyIDToken(t *testing.T) {
+
+	var user *User
+	var err error
+	if user, err = Google(""); err == nil {
 		t.Fatal("expected an error and got nothing")
 	}
 
 	if user != nil {
 		t.Fatal(user)
 	}
+}
 
-	// test with a wrong ID token
-	fmt.Println("Test with a wrong ID token")
-	user, err = Google("XYZ123")
-	if err == nil {
+func TestGoogleWithWrongIDToken(t *testing.T) {
+
+	var user *User
+	var err error
+	if user, err = Google("XYZ123"); err == nil {
 		t.Fatal("expected an error and got nothing")
 	}
 
